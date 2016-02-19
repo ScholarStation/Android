c877fda5a9158a3878288a8dd35d42ea4f667a17
bmp18 <bmp18@students.uwf.edu>
2016-02-19 16:38:55 -0600

    Working on Creating a Study Session

---
+++ ScholarStation/StudyResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ScholarStation
+{
+	public class StudyResponse
+	{
+		public StudyResponse ()
+		{
+			
+		}
+	}
+}
+
---
+++ ScholarStation/StudyUtility.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json;
+using RestSharp;
+using Android.Util;
+using System.Threading.Tasks;
+
+namespace ScholarStation
+{
+	public class StudyUtility
+	{
+		public Task<StudyResponse> StudyAsync(string userName, string course, string topic, string date, string time, string[] members){
+			return Task.Run (() => {
+				var client = new RestClient ("http://70.187.52.39:3000/StudyUtility/Create");
+				var req = new RestRequest (Method.POST);
+				string user = userName;
+				string c = course;
+				string t = topic;
+				string d = date;
+				string Time = time;
+				string[] m = members;
+				var studyObject = new LoginRequest {
+					username = user,
+					course = c,
+					topic = t,
+					date = d,
+					time = Time,
+					members = m,
+				};
+				var json = req.JsonSerializer.Serialize (studyObject);
+				req.AddParameter ("application/json; charset=utf-8", json, ParameterType.RequestBody);
+				Console.WriteLine ("HELLLOOOO!>>?????");
+				var returnStuff = client.Execute (req);
+				LoginResponse info = JsonConvert.DeserializeObject<LoginResponse> (returnStuff.Content);
+			}
+			);}
+			
+	}
+}
+
