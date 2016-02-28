92575fffb57e1e3cd621cf7a26d66b6e8597c5f7
bmp18 <bmp18@students.uwf.edu>
2016-02-28 12:41:23 -0600

    StudyUtility and StudyResponse done.

--- ScholarStation/StudyResponse.cs
+++ ScholarStation/StudyResponse.cs
@@ -4,15 +4,18 @@ namespace ScholarStation
 {
 	public class StudyResponse
 	{
-		public StudyResponse (string user)
-		{
-			this.username = user;
+		public string user{ get; set; }
+		public string KEY{ get; set; }
+		public string id{ get; set; }
+		public string course{ get; set; }
+		public string topic{ get; set; }
+		public string date{ get; set; }
+		public string time{ get; set; }
+		public string[] members { get; set; }
 
-		}
-		public string username{ get; set;}
 		public override string ToString ()
 		{
-			return string.Format ("[StudyResponse: username={0}]", username);
+			return string.Format ("[StudyResponse: user={0}, KEY={1}, id={2}, course={3}, topic={4}, date={5}, time={6}, members={7}]", user, KEY, id, course, topic, date, time, members);
 		}
 	}
 }
